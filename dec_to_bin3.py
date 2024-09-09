9ca838c4ffdbbf1ff76a12c0b0fe3f6e9a5b390d
I-Like-Lambda-Functions <173729232+I-Like-Lambda-Functions@users.noreply.github.com>
2024-09-09 22:27:39 +0200

    Update dec_to_bin3.py

--- dec_to_bin3.py
+++ dec_to_bin3.py
@@ -7,7 +7,9 @@ def dec_to_bin(number: int, return_type: rt = list) -> Sequence[int]:
         return return_type([0])
     else:
         bit_array = []
+		
         while number > 0:
+			
             bit_array.append(number % 2)
             number //= 2
-        return return_type(bit_array[::-1])
+		return return_type(bit_array[::-1])
