cfa0c819758b14c0b5341edb4527b7d1bd2a5247
I-Like-Lambda-Functions <173729232+I-Like-Lambda-Functions@users.noreply.github.com>
2024-09-10 10:45:20 +0200

    Update dec_to_bin4.py

--- dec_to_bin4.py
+++ dec_to_bin4.py
@@ -12,3 +12,6 @@ def dec_to_bin(number: int, return_type: rt = list) -> Sequence[int]:
         for i in range(ceil(log2(number + 1))):
             bit_array.append(1 if (number & pow(2, i)) > 0 else 0)
         return return_type(bit_array[::-1])
+
+num = int(input())
+print(dec_to_bin(num))
